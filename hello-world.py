c4a2d4f83f0b6def793f771fbc1acce9637e2572
Andrew McMillin <mcmillinap@gmail.com>
2022-11-12 09:15:25 -0700

    Adding first line

---
+++ hello-world.py
@@ -0,0 +1,9 @@
+text = 'I live and work in Arizona'
+print(text.upper())
+print(text.lower())
+print(text.swapcase())
+print(text)
+text - text.lower()
+print(text)
+print(text.upper().capitalize().swapcase)
+
---
+++ list.py
@@ -0,0 +1,8 @@
+my_list = ['Aslan', 'Clarusway', 2022]
+new_list1 = list(my_list)
+new_list2 = [my_list]
+
+print(new_list1)
+print(len(new_list1))
+print(new_list2)
+print(len(new_list2))
